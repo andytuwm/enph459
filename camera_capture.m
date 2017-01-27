97c8198e2daa40dfe3752b0b5493f772296b7412
Andy Tu <andytu@alumni.ubc.ca>
2017-01-27 11:24:03 -0800

    add comments to clarify properties used in script

--- camera_capture.m
+++ camera_capture.m
@@ -8,15 +8,19 @@ global cam
 global cam_config
 global img
 END_PROGRAM = 0;
+% Detect keypresses in the figure
 gcf
 set(gcf, 'KeyPressFcn', @myKeyPressFcn)
 
-%imaqhwinfo('winvideo')
+% Access and configure a device. Type imaqhwinfo('winvideo') for more
+% information on the cameras.
 
-% Access and configure a device.
-% cam = videoinput('winvideo', 1, 'RGB24_640x480');
-cam = videoinput('winvideo', 1, 'MJPG_640x480');
+% This is the configuration for the Logitech quick cam 
+cam = videoinput('winvideo', 1, 'RGB24_640x480');
+% This configuration is for your own webcam.
+% cam = videoinput('winvideo', 1, 'MJPG_640x480');
 cam_config = getselectedsource(cam);
+% configure camera settings
 get(cam_config)
 set(cam_config, 'Exposure', -8);
 set(cam_config, 'BacklightCompensation', 'off');
@@ -26,7 +30,6 @@ set(cam_config, 'ExposureMode', 'manual');
 % cam_config.Exposure = -8;
 % cam_config.ExposureMode = 'manual';
 % cam_config.WhiteBalanceMode = 'manual';
-% disp(cam_config)
 
 % configures camera to repeat triggers and have 1 frame per trigger. doing
 % so does not store frames to memory so the memory won't get fillede up
@@ -52,14 +55,17 @@ global cam_config
 disp('key is pressed:')
 disp(event.Key)
 if event.Key == 's'
+    % Stop the program
     global END_PROGRAM
     END_PROGRAM = 1;
     stop(cam)
     delete(cam)
     disp('program stopped.')
 elseif event.Key == 'c'
+    % Display current camera config
     disp(cam_config)
 elseif event.Key == 'w'
+    % Save current frame as png with capture-date-# convention
     global img
     num = 0;
     name = strcat('capture-', date, '-',num2str(num), '.png');
